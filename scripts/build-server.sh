e57308d40a58fe4a7e39902b3c093c0ac3d29a9e
keggin-CHN <109941504+keggin-CHN@users.noreply.github.com>
2026-02-06 19:30:25 +0800

    修复了依赖不够导致的闪退问题

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,4 +1,13 @@
 python -m nuitka --standalone --assume-yes-for-downloads \
+  --include-package=onnx \
+  --include-package=google.protobuf \
+  --include-package=onnxruntime \
+  --include-package=async_tasks \
+  --include-package=cv2 \
+  --include-package=numpy \
+  --include-package=tinyface \
+  --include-package=bottle \
+  --include-package-data=onnx \
   --include-data-files="src-python/models/*.onnx=models/" \
   --output-dir=out src-python/server.py
 
