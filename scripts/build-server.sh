8097b653355a9e40994c6b6effc898190ff9c571
keggin-CHN <109941504+keggin-CHN@users.noreply.github.com>
2026-02-08 10:46:08 +0800

    APP端的BUG

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -9,6 +9,7 @@ python -m nuitka --standalone --assume-yes-for-downloads \
   --include-package=tinyface \
   --include-package=bottle \
   --include-package-data=onnx \
+  --nofollow-import-to=onnxruntime.capi.onnxruntime_pybind11_state \
   --include-data-files="src-python/models/*.onnx=models/" \
   --output-dir=out src-python/server.py
 
